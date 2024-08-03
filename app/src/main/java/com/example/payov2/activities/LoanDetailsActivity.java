bc840c6eb6ceaedf763af3c8b6e9b26183ca77fe
JAMAN <155188517+JAMAN20@users.noreply.github.com>
2024-08-03 19:07:32 -0400

    I Get Started with LoanDetailsActivity design and solved some design issues with some screens.

---
+++ app/src/main/java/com/example/payov2/activities/LoanDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.payov2.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.payov2.R;
+
+public class LoanDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loan_details);
+    }
+}
